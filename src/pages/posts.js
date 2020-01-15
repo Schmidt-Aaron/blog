534556a46c76d5533fe0f84eb9de89cb5a7fef4e
schmidt-aaron <aschmidt808@gmail.com>
2020-01-14 23:53:55 -0800

    misc netlify fix

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,21 +1,21 @@
 // blog post list page
 import React from "react"
 import { Link } from "gatsby"
-import usePosts from "../hooks/use-Posts"
+// import usePosts from "../hooks/use-Posts"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import PostPreview from "../components/postPreview"
 
 const Posts = () => {
-  const posts = usePosts()
-
+  // const posts = usePosts()
+  //TODO fix posts query
   return (
     <Layout>
       <SEO title="Blog" />
       <h1>Blog</h1>
-      {posts.map(post => (
+      {/* {posts.map(post => (
         <PostPreview key={post.slug} post={post} />
-      ))}
+      ))} */}
     </Layout>
   )
 }
