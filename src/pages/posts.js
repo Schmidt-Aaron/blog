6e5c86a117d4f68941bfd2747f205182e8601f28
schmidt-aaron <aschmidt808@gmail.com>
2020-01-14 23:41:46 -0800

    undo hook commented out

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,52 +1,21 @@
 // blog post list page
 import React from "react"
-import { graphql, useStaticQuery, Link } from "gatsby"
-// import usePosts from "../hooks/use-Posts"
+import { Link } from "gatsby"
+import usePosts from "../hooks/use-Posts"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import PostPreview from "../components/postPreview"
 
 const Posts = () => {
-  //TODO fix static query to ge post data
-  // const data = useStaticQuery(graphql`
-  //   query {
-  //     allMdx {
-  //       nodes {
-  //         frontmatter {
-  //           title
-  //           slug
-  //           alt
-  //           date
-  //           status
-  //           image {
-  //             sharp: childImageSharp {
-  //               fluid(quality: 80, maxWidth: 100, maxHeight: 100) {
-  //                 ...GatsbyImageSharpFluid_withWebp
-  //               }
-  //             }
-  //           }
-  //         }
-  //         excerpt
-  //       }
-  //     }
-  //   }
-  // `)
-
-  // return data.allMdx.nodes.map(post => ({
-  //   title: post.frontmatter.title,
-  //   slug: post.frontmatter.slug,
-  //   image: post.frontmatter.image,
-  //   alt: post.frontmatter.alt,
-  //   excerpt: post.excerpt,
-  // }))
+  const posts = usePosts()
 
   return (
     <Layout>
       <SEO title="Blog" />
       <h1>Blog</h1>
-      {/* {posts.map(post => (
+      {posts.map(post => (
         <PostPreview key={post.slug} post={post} />
-      ))} */}
+      ))}
     </Layout>
   )
 }
