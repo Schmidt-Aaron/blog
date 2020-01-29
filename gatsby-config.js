87e10c337d34284057b618c35bdbda4e1f3a59d9
schmidt-aaron <aschmidt808@gmail.com>
2020-01-29 10:19:45 -0800

    test: load ga script in header vs footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
       options: {
         trackingId: googleAnalyticsID,
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
