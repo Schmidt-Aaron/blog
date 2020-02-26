57712681a3b8d5c98ecadcb744e77a39c21f6c23
schmidt-aaron <aschmidt808@gmail.com>
2020-02-25 22:12:30 -0800

    hero media query update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -46,11 +46,15 @@ const TextBox = styled("div")`
     font-size: 1.5rem;
   }
 @media screen and (max-width: ${BREAKPOINTS.md}{
-    h1{
+    h1 {
       font-size: 3rem;
+      color: white;
+    text-shadow: 3px 2px 2px #565656;
     }
     h2 {
       font-size: 1.5rem;
+      color: white;
+    text-shadow: 3px 2px 2px #565656;
     }
   }
 `
